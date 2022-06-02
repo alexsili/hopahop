4bf2325a54c7ef9ff0eb9614237984772de8c91b
alexsili <alex.sili24@gmail.com>
2022-06-02 20:52:12 +0300

    add description harcodate description

--- resources/views/layouts/partials/footer.blade.php
+++ resources/views/layouts/partials/footer.blade.php
@@ -17,28 +17,46 @@
             <div class="col-md-4">
                 <h3><strong>Social</strong></h3>
                 <ul class="list-unstyled footer-social">
-                    <li><a href="https://www.facebook.com/1HopaHop/" target="_blank" rel="noopener noreferrer"><i
-                                class="fab fa-facebook-square share"></i><strong> Facebook </strong></a></li>
-                    <li><a href="https://www.instagram.com/hopahop_official/" target="_blank" rel="noopener noreferrer">
-                            <i
-                                class="fab fa-instagram-square share"></i><strong> Instagram </strong></a></li>
-                    <li><a href="https://www.youtube.com/c/HopaHop" target="_blank" rel="noopener noreferrer"><i
-                                class="fab fa-youtube-square share"></i><strong>
-                                Youtube </strong></a></li>
-                    <li><a href="https://www.tiktok.com/@hopahop9" target="_blank" rel="noopener noreferrer"><i
-                                class="fab fa-tiktok share"></i><strong>
-                                TikTok </strong></a></li>
-                    <li><a href="https://twitter.com/HopaHop9" target="_blank" rel="noopener noreferrer"><i
-                                class="fab fa-twitter share"></i><strong>
-                                Twitter </strong></a></li>
+                    <li>
+                        <a href="https://www.facebook.com/HopaHop-Nursery-Rhymes-100656955132329" target="_blank"
+                           rel="noopener noreferrer">
+                            <i class="fab fa-facebook-square share"></i>
+                            <strong> Facebook </strong>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://www.instagram.com/hopahop_official/" target="_blank" rel="noopener noreferrer">
+                            <i class="fab fa-instagram-square share"></i>
+                            <strong> Instagram </strong>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://www.youtube.com/c/HopaHop?sub_confirmation=1" target="_blank"
+                           rel="noopener noreferrer">
+                            <i class="fab fa-youtube-square share"></i>
+                            <strong> Youtube </strong>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://www.tiktok.com/@hopahop9" target="_blank" rel="noopener noreferrer">
+                            <i class="fab fa-tiktok share"></i>
+                            <strong>TikTok </strong>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://twitter.com/HopaHop9" target="_blank" rel="noopener noreferrer">
+                            <i class="fab fa-twitter share"></i>
+                            <strong>Twitter</strong>
+                        </a>
+                    </li>
                 </ul>
             </div>
         </div>
         <div class="row">
             <div class="col-md-12 text-center">
                 <p class="small">
-                    Copyright &copy; HopaHop
-                    All Rights Reserved <i class="fa fa-heart text-danger" aria-hidden="true"></i> by <a
+                    Copyright &copy; <a href="{{url('/')}}"> HopaHop </a> <br>
+                    All Rights Reserved by <a
                         href="https://www.linkedin.com/in/alexandru-sili-b925511b8/" target="_blank">Alex Sili</a>
                 </p>
             </div>
